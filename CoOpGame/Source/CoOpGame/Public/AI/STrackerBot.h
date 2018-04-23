1cf3f3e0731b332cd22af6cdafea89b61e947ffd
Karlito <astipanovic23@gmail.com>
2018-04-23 19:17:56 +0200

    99. Setup Sound Effects Part 1

--- CoOpGame/Source/CoOpGame/Public/AI/STrackerBot.h
+++ CoOpGame/Source/CoOpGame/Public/AI/STrackerBot.h
@@ -8,6 +8,7 @@
 
 class USHealthComponent;
 class USphereComponent;
+class USoundCue;
 
 UCLASS()
 class COOPGAME_API ASTrackerBot : public APawn
@@ -67,9 +68,18 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
 	float ExplosionDamage;
 
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	float SelfDamageInterval;
+
 	FTimerHandle TimerHandle_SelfDamage;
 
 	void DamageSelf();
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	USoundCue* SelfDestructSound;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	USoundCue* ExplodeSound;
 
 public:	
 	// Called every frame
