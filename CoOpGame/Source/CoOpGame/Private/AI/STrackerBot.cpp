cf0377dc356ff2afdbdf626c9a114763ca0e18a7
KARLITO\User <astipanovic23@gmail.com>
2018-05-03 12:51:39 +0200

    105. Create the Pickup Class

--- CoOpGame/Source/CoOpGame/Private/AI/STrackerBot.cpp
+++ CoOpGame/Source/CoOpGame/Private/AI/STrackerBot.cpp
@@ -144,6 +144,8 @@ void ASTrackerBot::DamageSelf()
 
 void ASTrackerBot::NotifyActorBeginOverlap(AActor* OtherActor)
 {
+	Super::NotifyActorBeginOverlap(OtherActor);
+
 	if (!bStartedSelfDestruction && !bExploded)
 	{
 		ASCharacter* PlayerPawn = Cast<ASCharacter>(OtherActor);
